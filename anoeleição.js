9e11f327d14546f5a286ad1b4991494fec304e47
Samuel jorge matheus <115334920+samueljmatheus@users.noreply.github.com>
2023-02-01 19:54:26 -0300

    update

---
+++ anoeleição.js
@@ -0,0 +1,11 @@
+var AnoNascimento = 2022;
+idade = 2023 - AnoNascimento
+
+if (idade<16){
+    console.log("Inapto para o voto!!!")
+} 
+else if (idade>=16 && idade<70) {
+    console.log("Voto obrigatório!!!")
+} else {
+    console.log("Voto Opcional!!")
+}
